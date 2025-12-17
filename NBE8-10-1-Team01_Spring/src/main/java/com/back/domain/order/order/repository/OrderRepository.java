96c90e552ab6cc0a2ad418560de20f1873754882
Minji-032 <5h6vmm@gmail.com>
2025-12-17 15:36:30 +0900

    [Fix/#B18-25] 주문 합침 추가

--- NBE8-10-1-Team01_Spring/src/main/java/com/back/domain/order/order/repository/OrderRepository.java
+++ NBE8-10-1-Team01_Spring/src/main/java/com/back/domain/order/order/repository/OrderRepository.java
@@ -3,10 +3,14 @@ package com.back.domain.order.order.repository;
 import com.back.domain.order.order.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     // Orders.customer.id 를 의미 (중첩 프로퍼티)
     List<Orders> findByCustomer_IdOrderByCreateDateDesc(Long customerId);
+    Optional<Orders> findTopByCustomer_IdAndCreateDateBetweenOrderByCreateDateDesc(
+            Long customerId, LocalDateTime start, LocalDateTime end
+    );
 }
