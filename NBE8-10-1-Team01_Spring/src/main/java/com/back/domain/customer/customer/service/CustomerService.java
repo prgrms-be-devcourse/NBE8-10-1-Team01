40e891651f3fcb90ea248f96fe60a8aa6359a87f
Minji-032 <5h6vmm@gmail.com>
2025-12-18 15:38:03 +0900

    [Fix/#B18-43] 에러 헨들링 방식 변경

--- NBE8-10-1-Team01_Spring/src/main/java/com/back/domain/customer/customer/service/CustomerService.java
+++ NBE8-10-1-Team01_Spring/src/main/java/com/back/domain/customer/customer/service/CustomerService.java
@@ -4,6 +4,7 @@ import com.back.domain.customer.customer.dto.CustomerLoginRequest;
 import com.back.domain.customer.customer.dto.CustomerLoginResponse;
 import com.back.domain.customer.customer.entity.Customer;
 import com.back.domain.customer.customer.repository.CustomerRepository;
+import com.back.global.exception.customer.CustomerNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,9 +37,7 @@ public class CustomerService {
 
         // 고객 이메일 체크 - DB에 존재하는지 확인
         Customer customer = customerRepository.findByEmail(email)
-                .orElseThrow(() ->
-                        new IllegalArgumentException("존재하지 않는 이메일입니다.")
-                );
+                .orElseThrow(() -> new CustomerNotFoundException("존재하지 않는 이메일입니다."));
 
         // 고객 리다이렉트
         return CustomerLoginResponse.builder()
