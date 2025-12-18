14e8aec998d562b49cf55f47e9fb7bc44f726a03
bindoong01 <mailsky9687@gmail.com>
2025-12-18 10:35:16 +0900

    [Fix/#B18-66] 상품 수정 , 삭제 json 응답

---
+++ NBE8-10-1-Team01_Spring/src/main/java/com/back/domain/product/product/dto/ProductDeleteResponse.java
@@ -0,0 +1,14 @@
+package com.back.domain.product.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDeleteResponse {
+    private String message;
+}
