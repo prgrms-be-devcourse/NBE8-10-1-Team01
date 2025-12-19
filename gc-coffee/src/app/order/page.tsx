cb7ee866eaa820afc41b3ed91b59763c6351efd9
gurum505 <sayhojeong505@gmail.com>
2025-12-19 16:47:32 +0900

    [refactor] 장바구니 내역 리팩토링

--- gc-coffee/src/app/order/page.tsx
+++ gc-coffee/src/app/order/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useRef, useState } from "react";
 import Script from 'next/script';
 import { useRouter } from "next/navigation";
+import { ProductDto, OrderDto } from "@/type/item";
+import { apiFetch } from "@/lib/backend/client";
 
 declare global {
   interface Window {
@@ -10,17 +12,11 @@ declare global {
   }
 }
 
-function Payment({ cart, totalAmount }: {
-  cart: Order[],
-  totalAmount: number
-}) {
-  const router = useRouter();
-
+function AddressForm() {
   const [postalCode, setPostalCode] = useState('');
   const [address1, setAddress1] = useState('');
   const [address2, setAddress2] = useState('');
   const inputRef = useRef(null);
-
   const handleAddressSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
     if (!window.daum || !window.daum.Postcode) {
@@ -58,6 +54,66 @@ function Payment({ cart, totalAmount }: {
     }
   };
 
+  return (
+    <>
+      <Script
+        src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
+        strategy="afterInteractive"
+      />
+      {/* 주소 검색 그룹 */}
+      <label className="block text-sm font-medium text-gray-700" htmlFor="postalCode">주소</label>
+      <div className="flex space-x-3">
+        <input
+          type="text"
+          id="postalCode"
+          name="postalCode"
+          className="p-3 border border-gray-300 rounded-md shadow-sm w-1/3"
+          placeholder="우편번호"
+          readOnly
+          required
+          value={postalCode}
+        />
+        <button
+          type="button"
+          onClick={handleAddressSearch}
+          className="bg-gray-500 hover:bg-gray-600 text-white font-medium py-3 px-4 rounded-md transition duration-150 w-2/3 cursor-pointer"
+        >
+          우편번호 검색
+        </button>
+      </div>
+
+      {/* 기본 주소 */}
+      <input
+        type="text"
+        id="address1"
+        name="address1"
+        className="mt-1 p-3 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-amber-500 focus:border-amber-500"
+        placeholder="기본 주소 (검색 결과)"
+        readOnly
+        required
+        value={address1}
+      />
+
+      {/* 상세 주소 */}
+      <input
+        type="text"
+        id="address2"
+        name="address2"
+        className="mt-1 p-3 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-amber-500 focus:border-amber-500"
+        placeholder="상세 주소 (예: 101동 202호)"
+        required
+        ref={inputRef}
+      />
+    </>
+  );
+}
+
+function Payment({ cart }: {
+  cart: OrderDto[]
+}) {
+  const router = useRouter();
+  const totalAmount = cart.reduce((acc, item) => acc + item.count * Number(item.product.price), 0);
+  
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const form = e.target as HTMLFormElement;
@@ -65,57 +121,47 @@ function Payment({ cart, totalAmount }: {
     const emailInput = form.elements.namedItem("email") as HTMLInputElement;
     emailInput.value = emailInput.value.trim();
 
+    const postalCodeInput = form.elements.namedItem("postalCode") as HTMLInputElement;
+    postalCodeInput.value = postalCodeInput.value.trim();
+
     const addressInput = form.elements.namedItem("address2") as HTMLInputElement;
     addressInput.value = addressInput.value.trim();
 
-    if(postalCode === "" || addressInput.value === "" || cart.length === 0){
+    if (postalCodeInput.value === "" || addressInput.value === "" || cart.length === 0) {
       alert("필수사항을 입력해주십시오.")
-      console.log("postal"+postalCode);
-      console.log("address2"+address2);
-      console.log("cart"+cart);
-      return; 
+      return;
     }
 
-    const SERVER_URL = "http://localhost:8080";
-    fetch(SERVER_URL + "/api/orders", {
-      headers:{'Content-Type':'application/json'},
+    apiFetch("/api/orders", {
+      headers: { 'Content-Type': 'application/json' },
       method: "POST",
       body: JSON.stringify({
         customer: {
           email: emailInput.value,
           address: addressInput.value,
-          postcode: postalCode
+          postcode: postalCodeInput.value
         },
         products: cart.map((item) => ({
           productId: item.product.productId,
           count: item.count
         }))
       })
-    }).then(res => {
-      if (!res.ok) throw new Error("주문에 실패했습니다.");
-      return res.json();
     })
-      .then(res => alert("결제가 완료되었습니다."))
-      .catch(err => {
-        console.error(err);
-        alert("에러가 발생했습니다: " + err.message);
-      });
+    .then(res => alert("결제가 완료되었습니다."))
+    .catch(err => {
+      console.error(err);
+      alert("에러가 발생했습니다: " + err.message);
+    });
     router.replace("/");
   };
 
   return (
     <>
-      <Script
-        src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
-        strategy="afterInteractive" 
-      />
       <section className="py-16 px-4 bg-white border-t border-gray-100">
         <div className="max-w-xl mx-auto">
           <h2 className="text-3xl font-semibold mb-8 text-amber-900 border-b pb-2">배송 및 결제 정보</h2>
 
           <form onSubmit={handleSubmit} className="flex flex-col space-y-4 p-6 border border-gray-200 rounded-lg shadow-xl bg-white">
-
-            {/* 이메일 입력 */}
             <label className="block text-sm font-medium text-gray-700 mt-2" htmlFor="email">이메일 주소</label>
             <input
               type="email"
@@ -126,50 +172,8 @@ function Payment({ cart, totalAmount }: {
               required
             />
 
-            {/* 주소 검색 그룹 */}
-            <label className="block text-sm font-medium text-gray-700" htmlFor="postalCode">주소</label>
-            <div className="flex space-x-3">
-              <input
-                type="text"
-                id="postalCode"
-                className="p-3 border border-gray-300 rounded-md shadow-sm w-1/3"
-                placeholder="우편번호"
-                readOnly
-                required
-                value={postalCode}
-              />
-              <button
-                type="button"
-                onClick={handleAddressSearch}
-                className="bg-gray-500 hover:bg-gray-600 text-white font-medium py-3 px-4 rounded-md transition duration-150 w-2/3 cursor-pointer"
-              >
-                우편번호 검색
-              </button>
-            </div>
+            <AddressForm />
 
-            {/* 기본 주소 */}
-            <input
-              type="text"
-              id="address1"
-              className="mt-1 p-3 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-amber-500 focus:border-amber-500"
-              placeholder="기본 주소 (검색 결과)"
-              readOnly
-              required
-              value={address1}
-            />
-
-            {/* 상세 주소 */}
-            <input
-              type="text"
-              id="address2"
-              name="address2"
-              className="mt-1 p-3 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-amber-500 focus:border-amber-500"
-              placeholder="상세 주소 (예: 101동 202호)"
-              required
-              ref={inputRef}
-            />
-
-            {/* 최종 결제 버튼 */}
             <button
               type="submit"
               className="mt-6 bg-amber-700 hover:bg-amber-800 text-white font-semibold py-3 rounded-md transition duration-200 shadow-lg cursor-pointer"
@@ -185,12 +189,13 @@ function Payment({ cart, totalAmount }: {
   );
 }
 
-function Cart({ cart, totalAmount ,addToCart,minusToCart}: {
-  cart: Order[];
-  totalAmount: number;
-  addToCart: (product: Product) => void;
-  minusToCart: (product: Product) => void;
+function Cart({ cart, addToCart, minusToCart }: {
+  cart: OrderDto[];
+  addToCart: (product: ProductDto) => void;
+  minusToCart: (product: ProductDto) => void;
 }) {
+  const totalAmount = cart.reduce((acc, item) => acc + item.count * Number(item.product.price), 0);
+
   return (
     <section className="py-16 px-4 bg-white">
       <div className="max-w-4xl mx-auto">
@@ -211,10 +216,10 @@ function Cart({ cart, totalAmount ,addToCart,minusToCart}: {
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{item.product.name}</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-500">
                   <button className="px-4 bg-amber-100 text-stone-700 py-2 rounded-lg text-sm font-medium hover:bg-amber-200 transition mx-2 cursor-pointer"
-                  onClick={()=>minusToCart(item.product)}>-</button>
+                    onClick={() => minusToCart(item.product)}>-</button>
                   {item.count}개
                   <button className="px-4 bg-amber-100 text-stone-700 py-2 rounded-lg text-sm font-medium hover:bg-amber-200 transition mx-2 cursor-pointer"
-                  onClick={()=>addToCart(item.product)}>+</button>
+                    onClick={() => addToCart(item.product)}>+</button>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-right text-gray-500">{item.product.price.toLocaleString()}원</td>
                 <td className="px-6 py-4 whitespace-nowrap text-sm text-right font-semibold text-gray-800">{(item.count * Number(item.product.price)).toLocaleString()}원</td>
@@ -233,45 +238,17 @@ function Cart({ cart, totalAmount ,addToCart,minusToCart}: {
   );
 }
 
-function CartAndPayment({ cart,addToCart,minusToCart }: {
-  cart: Order[];
-  addToCart: (product: Product) => void;
-  minusToCart: (product: Product) => void;
-}) {
+function ItemList({ addToCart }: { addToCart: (product: ProductDto) => void }) {
+  const [productList, setProductList] = useState<ProductDto[]>([]);
 
-  const totalAmount = cart.reduce((acc, item) => acc + item.count * Number(item.product.price), 0);
-
-  return (
-    <>
-      <Cart cart={cart} totalAmount={totalAmount} addToCart={addToCart} minusToCart={minusToCart} />
-      <Payment cart={cart} totalAmount={totalAmount} />
-    </>
-  );
-}
-
-interface Product {
-  productId: number;
-  name: string;
-  description: string;
-  image: string;
-  price: string;
-}
-
-function ItemList({ addToCart }: any) {
-  const [productList, setProductList] = useState<Product[]>([]);
-
-  const SERVER_URL = "http://localhost:8080";
   const headers = new Headers();
   headers.set("Content-Type", "application/json; charset=utf-8");
   useEffect(() => {
-    fetch(SERVER_URL + "/api/products", { headers: headers })
-      .then(res => res.json())
+    apiFetch("/api/products", { headers: headers })
       .then(res => setProductList(res.data));
-
   }, []);
 
-
-  const onClick = (product: Product) => {
+  const onClick = (product: ProductDto) => {
     addToCart(product);
   };
 
@@ -287,7 +264,6 @@ function ItemList({ addToCart }: any) {
                 // 여기에 flex-shrink-0 클래스가 반드시 있어야 가로로 겹치지 않음
                 className={`flex-shrink-0 w-64 p-5 rounded-xl shadow-lg border border-amber-200`}
               >
-                <div className="h-24 w-full mb-3 rounded-md bg-white/70 flex items-center justify-center text-4xl text-amber-800">☕</div>
                 <h3 className="text-lg font-bold text-amber-800 mb-1">{item.name}</h3>
                 <p className="text-sm text-gray-600 mb-4">{item.description}</p>
                 <button className="w-full bg-amber-600 text-white py-2 rounded-lg text-sm font-medium hover:bg-amber-700 transition cursor-pointer"
@@ -303,15 +279,10 @@ function ItemList({ addToCart }: any) {
   );
 }
 
-interface Order {
-  product: Product
-  count: number;
-}
-
 export default function Home() {
-  const [cart, setCart] = useState<Order[]>([]);
+  const [cart, setCart] = useState<OrderDto[]>([]);
 
-  const addToCart = (product: Product) => {
+  const addToCart = (product: ProductDto) => {
     const isExist = cart.find((item) => item.product.productId === product.productId);
     if (isExist) {
       setCart(cart.map((cartItem) => {
@@ -325,27 +296,30 @@ export default function Home() {
     }
   };
 
-  const minusToCart = (product: Product) => {
+  const minusToCart = (product: ProductDto) => {
     const isExist = cart.find((item) => item.product.productId === product.productId);
     if (isExist) {
       const isOne = cart.find((item) => item.product.productId === product.productId && item.count === 1);
-      if(!isOne){
+      if (!isOne) {
         setCart(cart.map((cartItem) => {
           if (cartItem.product.productId == product.productId) {
             return { ...cartItem, count: cartItem.count - 1 };
           }
           return cartItem;
         }));
-      }else{
+      } else {
         setCart(cart.filter((cartItem) => cartItem.product.productId != product.productId));
       }
     }
   };
 
+  const totalAmount = cart.reduce((acc, item) => acc + item.count * Number(item.product.price), 0);
+
   return (
     <div className="min-h-screen bg-amber-50">
       <ItemList addToCart={addToCart} />
-      <CartAndPayment cart={cart} addToCart={addToCart} minusToCart={minusToCart}/>
+      <Cart cart={cart} addToCart={addToCart} minusToCart={minusToCart} />
+      <Payment cart={cart} />
     </div>
   );
 }
--- gc-coffee/src/type/item.ts
+++ gc-coffee/src/type/item.ts
@@ -1,7 +1,13 @@
 export type ProductDto  = {
+    productId: number;
     name: string;
     description: string;
     image: string;
     price: string;
 }
   
+export type OrderDto ={
+  product:ProductDto
+  count: number;
+}
+
